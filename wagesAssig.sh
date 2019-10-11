782d722e0c5fb06ab1be33eb8669a3201048413e
John Caffarelli <jcaffare@nd.edu>
2019-10-10 21:47:47 -0400

    Task 2.3 pipe

--- wagesAssig.sh
+++ wagesAssig.sh
@@ -8,4 +8,5 @@ cat "$1" | sed '1d' | cut -d , -f 1,2,4 | sort -t "," -k3,3 | tail -n 1
 #2 Return gender, yearExperience and wage for lowest earner
 cat "$1" | sed '1d' | cut -d , -f 1,2,4 | sort -t "," -k3,3 | head -n 1
 #3 Return number of females in top 10 earners of data set
+cat "$1" | sed '1d' | cut -d , -f 1,2,4 | sort -t "," -k3,3 | tail | grep 'female' | wc -l 
 #Task 3
