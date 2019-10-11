a5b541d12b13efcae7c8376112fea9fae69742d9
John Caffarelli <jcaffare@nd.edu>
2019-10-10 20:45:41 -0400

    Adding instructions for task 2

--- wagesAssig.sh
+++ wagesAssig.sh
@@ -5,6 +5,8 @@ echo "Question 1: see new .txt file 'gender-yearsExperience.txt'"
 cat $1 | sed '1d' | cut -d , -f 1,2| tr "," " " | sort -t " " -k1,1 -k2,2n | uniq > gender-yearsExperience.txt
 
 #Task 2
-
+#1 Return gender, yearExperience and wage for highest earner
+#2 Return gender, yearExperience and wage for lowest earner
+#3 Return number of females in top 10 earners of data set
 
 #Task 3
